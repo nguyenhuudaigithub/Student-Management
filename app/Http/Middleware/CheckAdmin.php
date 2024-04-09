3cec41ef3b5dccd224c40e1c282a5e013d1f9530
NguyenHuuDai <nguyenhuudaihs@icloud.com>
2024-04-09 17:44:47 +0700

    role

---
+++ app/Http/Middleware/CheckAdmin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CheckAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (!Auth::user()->isAdmin) {
+            return redirect('/')->with('error', 'New administrator with access!');
+        }
+
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+Route::middleware(['App\Http\Middleware\Authenticate'])->group(function () {
+    Route::middleware([
+        'auth:sanctum',
+        config('jetstream.auth_session'),
+        'verified',
+    ])->group(function () {
+        Route::get('/dashboard', function () {
+            return view('dashboard');
+        })->name('dashboard');
+    });
+
+    Route::middleware(['App\Http\Middleware\CheckAdmin'])->group(function () {
+        Route::get('/admin', function () {
+            // Chỉ các admin mới có thể truy cập route này
+        });
+    });
 });
